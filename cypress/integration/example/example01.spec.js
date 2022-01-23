21829cef8ec858b5d306ac1043b6a51537af0fc5
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-23 15:16:40 +0300

    config viewport, add example01 integ.

---
+++ cypress/integration/example/example01.spec.js
@@ -0,0 +1,19 @@
+/// <reference types = "Cypress"/>
+
+context("4 islem", () => {
+    const s1 = 9
+    const s2 = 6
+    let sonuc
+    it("Toplama", () => {
+        sonuc = s1 + s2
+        assert.equal(sonuc, 15, "Toplama sonuc kontrolu ")
+    })
+    it("Cıkarma", () => {
+        sonuc = s1 - s2
+        assert.isTrue(sonuc === 0, "Toplama sonuc kontrolu ")
+    })
+    it("Text kontrolu", () => {
+        const text1 = "Cypress io"
+        expect(text1).to.have.string("io")
+    })
+})
